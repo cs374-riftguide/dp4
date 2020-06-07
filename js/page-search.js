cs374-riftguide/dp4

commit 4fa0bf0d9b8ab9f2a6d15ceca8806a14430434d7

refactor: Extract tierFilterTypes
Extract `tierFilterTypes` into a separate constant, and use it to
initialize `activeTierFilters`. This allows us to avoid manually
initializing `activeTierFilters` when the component is mounted.

js/page-search.js
@@ -5,6 +5,36 @@
 
 /// <reference path="./index.d.ts" />
 
+const tierFilterTypes = {
+  iron: {
+    name: "Iron",
+  },
+  bronze: {
+    name: "Bronze",
+  },
+  silver: {
+    name: "Silver",
+  },
+  gold: {
+    name: "Gold",
+  },
+  platinum: {
+    name: "Platinum",
+  },
+  diamond: {
+    name: "Diamond",
+  },
+  master: {
+    name: "Master",
+  },
+  grandmaster: {
+    name: "Grandmaster",
+  },
+  challenger: {
+    name: "Challenger",
+  },
+};
+
 Vue.component("page-search", {
   props: {
     guides: {
@@ -14,38 +44,10 @@ Vue.component("page-search", {
   },
   data() {
     return {
-      activeTierFilters: [],
+      activeTierFilters: Object.values(tierFilterTypes).map(({ name }) => name),
       fuse: null,
       searchText: "",
-      tierFilterTypes: {
-        iron: {
-          name: "Iron",
-        },
-        bronze: {
-          name: "Bronze",
-        },
-        silver: {
-          name: "Silver",
-        },
-        gold: {
-          name: "Gold",
-        },
-        platinum: {
-          name: "Platinum",
-        },
-        diamond: {
-          name: "Diamond",
-        },
-        master: {
-          name: "Master",
-        },
-        grandmaster: {
-          name: "Grandmaster",
-        },
-        challenger: {
-          name: "Challenger",
-        },
-      },
+      tierFilterTypes,
     };
   },
   computed: {
@@ -104,9 +106,6 @@ Vue.component("page-search", {
     },
   },
   mounted() {
-    // Initially, all filters are active
-    this.activeTierFilters = Object.keys(this.tierFilterTypes);
-
     // Initialize Fuse.js
     this.fuse = new Fuse(this.guides, {
       keys: [

commit 953215c71a82e093f03558aa2abe1e3e7c52f516

fix: Properly enable all tier filters upon load
This fixes a bug introduced in 4fa0bf0d9b8ab9f2a6d15ceca8806a14430434d7,
which populated the `activeTierFilters` array with invalid values.
The correct values should be the keys of `tierFilterTypes`, but I
accidentally used the values instead :(

js/page-search.js
@@ -44,7 +44,7 @@ Vue.component("page-search", {
   },
   data() {
     return {
-      activeTierFilters: Object.values(tierFilterTypes).map(({ name }) => name),
+      activeTierFilters: Object.keys(tierFilterTypes),
       fuse: null,
       searchText: "",
       tierFilterTypes,