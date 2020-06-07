ae7d651125a4237e3dd2e6a1e98a17d75a318fcf
Yehyoung Kang <pastelmind@kaist.ac.kr>
2020-06-08 00:07:37 +0900

    perf: Cache searched & filtered data separately
    Split the searching & filtering into two stages. The computation of
    'searchResults' is separated into two computed fields:
    
    - searchedGuides: Searched with Fuse.js, but not filtered or transformed
      yet
    - filteredSearchResults: Filtered and transformed
    
    This allows Vue.js to cache the search result when the user changes the
    filters--a minor optimization, but with possibly significant payoff in
    the future.

--- js/app.js
+++ js/app.js
@@ -41,17 +41,25 @@ const vm = new Vue({
   },
   computed: {
     /**
-     * @returns {SearchResultItem[]}
+     * Returns guides that match the current search text (`searchText`).
+     * @returns {Guide[]}
      */
-    searchResults() {
+    searchedGuides() {
       // Show nothing if the search text is less than 2 characters long
       if (!this.searchText || this.searchText.length < 2) return [];
 
-      /** @type {Guide[]} */
-      const results = this.fuse
+      return this.fuse
         .search(this.searchText)
         .map((resultItem) => resultItem.item);
-      return results
+    },
+    /**
+     * Returns search results filtered by currently active tier filters.
+     * @returns {SearchResultItem[]}
+     */
+    filteredSearchResults() {
+      /** @type {Guide[]} */
+      const searchedGuides = this.searchedGuides;
+      return searchedGuides
         .filter((guide) => this.activeTierFilters.includes(guide.tier))
         .map((guide) => {
           // Drop the 'content' key from the searchResultItem
